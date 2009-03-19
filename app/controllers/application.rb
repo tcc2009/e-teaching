858e86c2874998e2d8913dadf7d13918c4a75e56
Hebert Santana <hebert.uesc@gmail.com>
2009-03-19 18:48:25 -0300

    Primeira versão - Compartilhamento de tela Flex ainda não funciona, por isso está funcionando via html.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,19 +1,20 @@
 require 'yaml'
-require 'screen'
+require 'socket'
 
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
+	attr_accessor :teachers_screen
   
   before_filter :configure_network
-  before_filter :configure_screen
+
 
   def configure_network
     @multicast = YAML::load(File.open("#{RAILS_ROOT}/config/network.yml","r"))
-  end
-  def configure_screen
-    @teachers_screen = Screen.new
-    DRb.start_service('druby://192.168.1.72:4000',@teachers_screen)
+    @@multicast_addr = "225.4.5.6" 
+    @@port= 5000
+    @socket = UDPSocket.open 
+    @socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_TTL, [1].pack('i')) 
   end
 
 end
--- app/controllers/screen_controller.rb
+++ app/controllers/screen_controller.rb
@@ -1,6 +1,7 @@
 class ScreenController < ApplicationController
+
   def index
-    if @teachers_screen == true
+    if $teachers_screen == true
       @state = "Tela Compartilhada"
     else
       @state = "Tela não Compartilhada"
@@ -8,12 +9,16 @@ class ScreenController < ApplicationController
   end
   
   def share
-    @teachers_screen.share = true
-    redirect_to :action => "index"
+    @socket.send('yes', 0, @@multicast_addr, @@port) 
+    @socket.close
+    $teachers_screen = true
+    redirect_to :action => "index" 
   end
   
   def dont_share
-    @teachers_screen.share = false
+    @socket.send('no', 0, @@multicast_addr, @@port) 
+    @socket.close
+    $teachers_screen = false
     redirect_to :action => "index"
   end
   
