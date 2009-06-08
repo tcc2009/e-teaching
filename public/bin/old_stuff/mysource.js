bf0e55e7d8323e7c435455fa070897e67cd496cd
Pablo Barboza <tcc2009_1@yahoogrupos.com.br>
2009-06-08 10:02:54 -0300

    Update the flex UI in public/bin

--- public/bin/old_stuff/mysource.js
+++
@@ -1,5 +0,0 @@
-document.write("<object id='Main' classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0' height='100%' width='100%'>");
-document.write("<param name='src' value='Pomodo.swf'/>");
-document.write("<embed name='main' src='Main.swf' pluginspage='http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash' height='100%' width='100%'/>");
-document.write("</object>");
-
